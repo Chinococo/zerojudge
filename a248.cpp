d36f6fb6f8da9c6f29b937e03e81009600a59cb0
Chinococo <58754099+Chinococo@users.noreply.github.com>
2021-06-24 07:52:54 +0800

    Add files via upload

--- a248.cpp
+++ a248.cpp
@@ -1,16 +1,42 @@
 #include <bits/stdc++.h>
-#define max_size 101
 using namespace std;
 int main() {
-    long long a,b,c;
-    while(cin>>a>>b>>c)
+    int x,y,z,w,n,m,t;
+    cin>>t;
+    while(t--)
     {
-        cout<<a/b<<'.';
-        while(c--)
-            a%=b,a*=10,cout<<a/b;
-        cout<<endl;
+        cin>>x>>y>>z>>w>>n>>m;
+        string cmd;
+        cin.ignore();
+        getline(cin,cmd);
+        stringstream ss;
+        ss<<cmd;
+        string temp;
+        int bad=0;
+        //cout<<m<<endl;
+        while(ss>>temp)
+        {
+            m-=bad;
+            if(m<=0)
+                break;
+            if(temp=="1")
+                m+=x;
+            else if(temp=="2")
+                m+=y;
+            else if(temp=="3")
+                m-=z;
+            else if(temp=="4")
+                m-=w;
+            if(m<=0)
+                break;
+            if(temp=="4")
+                bad+=n;
+            //cout<<m<<endl;
+        }
+        if(m<=0)
+            cout<<"bye~Rabbit"<<endl;
+        else
+            cout<< m<<"g"<<endl;
     }
-
-
 
 }
